5c7f7bfbcdda43f4b2733185f22fe0a983b09882
Coy <coylook@outlook.com>
2018-05-02 10:56:27 +0800

    Update install-bbr.sh

--- install-bbr.sh
+++ install-bbr.sh
@@ -3,3 +3,6 @@ yum install wget
 wget –no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 chmod +x bbr.sh
 ./bbr.sh
+# remove kernel
+# rpm -qa|grep kernel
+# yum remove kernel_name
