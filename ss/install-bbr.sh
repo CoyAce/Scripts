09bdbc661b6217eec436a55c777c26ab2117eaf5
Coy <coylook@outlook.com>
2018-09-22 15:12:24 +0800

    Delete install-bbr.sh

--- ss/install-bbr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum install wget
-wget –no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
-chmod +x bbr.sh
-./bbr.sh
-# remove kernel
-# rpm -qa|grep kernel
-# yum remove kernel_name
