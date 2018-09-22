c8e769024795c462006b3f4e48416cd828d3971f
Coy <coylook@outlook.com>
2018-09-22 13:13:01 +0800

    auto set ip

--- ss/install_ss.sh
+++ ss/install_ss.sh
@@ -1,6 +1,7 @@
 yum install python-setuptool
 sudo easy_install pip
 sudo pip install shadowsocks
+ip=`ifconfig eth0| grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 echo '''
 {
     "server":"host",
@@ -10,5 +11,5 @@ echo '''
     "timeout":600,
     "method":"aes-256-cfb"
 }
-''' > shadowsocks.json
+'''|sed -e "s/\"server\".*/\"server\":\"$ip\"/"> shadowsocks.json
 mv shadowsocks.json /etc/shadowsocks.json
