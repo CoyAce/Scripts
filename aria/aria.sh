ae6308e22cd0f847991eeb7bd7210823fdea5774
Coy <akeycoy@gmail.com>
2024-01-04 09:34:57 +0800

    Update aria.sh
    add tracker

--- aria/aria.sh
+++ aria/aria.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nohup aria2c --conf-path /etc/aria2.conf &
+trackers=`wget -qO- https://trackerslist.com/best_aria2.txt`
+nohup aria2c --conf-path /etc/aria2.conf --bt-tracker=$trackers &
