2352f2cd09261306c4b3adcc47def6854563b5a3
CoyAce <hlliu2114@mails.jlu.edu.cn>
2025-04-30 18:16:14 +0800

    更新登录脚本，覆盖密钥直接登录的case

--- login.sh
+++ login.sh
@@ -2,7 +2,10 @@
 # 设置永不超时
 set timeout -1
 spawn ssh root@coyace.top
-expect "*password:"
-send "password\r"
+expect {
+  "*password:" {send "password\r"}
+  #通过ssh-copy-id添加密钥后直接登录成功
+  "Welcome" {}
+}
 # 权限交给控制台
 interact
